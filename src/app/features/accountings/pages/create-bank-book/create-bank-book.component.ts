d2e356f1a139de057958dd2d6882660eaf98c82d
Fangan508 <kanedaa@gmx.net>
2025-11-02 21:09:47 +0100

    Added style for create bank book component

--- src/app/features/accountings/pages/create-bank-book/create-bank-book.component.ts
+++ src/app/features/accountings/pages/create-bank-book/create-bank-book.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CreateBankBookStepComponent } from "../../components/create-bank-book/create-bank-book-step/create-bank-book-step.component";
 import { CreateBankBookStepperComponent } from "../../components/create-bank-book/create-bank-book-stepper/create-bank-book-stepper.component";
 import { CreateBankBookActionsComponent } from "../../components/create-bank-book/create-bank-book-actions/create-bank-book-actions.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ek-create-bank-book',
-  imports: [CreateBankBookStepComponent, CreateBankBookStepperComponent, CreateBankBookActionsComponent],
+  imports: [CommonModule, CreateBankBookStepComponent, CreateBankBookStepperComponent, CreateBankBookActionsComponent],
   templateUrl: './create-bank-book.component.html',
   styleUrl: './create-bank-book.component.scss'
 })
