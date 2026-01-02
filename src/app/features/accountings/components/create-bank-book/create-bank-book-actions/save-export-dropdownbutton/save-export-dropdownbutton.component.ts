8124edaccad635bd4222681aee01ad7022773ba9
Fangan508 <kanedaa@gmx.net>
2026-01-02 19:57:41 +0100

    feat: added styling for export button

--- src/app/features/accountings/components/create-bank-book/create-bank-book-actions/save-export-dropdownbutton/save-export-dropdownbutton.component.ts
+++ src/app/features/accountings/components/create-bank-book/create-bank-book-actions/save-export-dropdownbutton/save-export-dropdownbutton.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { ExportOption } from '@ek/features/accountings/models/export-option.model';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'ek-save-export-dropdownbutton',
-  imports: [MatFormField, MatLabel, MatSelectModule],
+  imports: [MatButtonModule, MatIcon, MatFormField, MatLabel, MatSelectModule, MatMenuModule],
   templateUrl: './save-export-dropdownbutton.component.html',
   styleUrl: './save-export-dropdownbutton.component.scss'
 })
